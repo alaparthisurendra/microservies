ba63f4a5e64e10c8cd66f812987b107019daa44b
40003758 <40003758@BLTSP02835.lnties.com>
2019-03-05 18:04:06 +0530

    modified catalog controller

--- src/main/java/com/moviecatalogservice/controller/CatalogController.java
+++ src/main/java/com/moviecatalogservice/controller/CatalogController.java
@@ -1,6 +1,5 @@
 package com.moviecatalogservice.controller;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -8,12 +7,13 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
-
 import com.moviecatalogservice.model.CatalogItem;
 import com.moviecatalogservice.model.Movie;
 import com.moviecatalogservice.model.Rating;
@@ -36,8 +36,9 @@ public class CatalogController {
 		//WebClient.Builder builder=WebClient.builder();
 		//assume that this data cmg from rating api
 		List<Rating> ratings=Arrays.asList(
-				new Rating("111",4),
-				new Rating("112",3)
+				new Rating("111",3),
+				new Rating("112",4),
+				new Rating("113",2)
 				);
 		
 		// step:1 get all rated movie ids
@@ -49,7 +50,9 @@ public class CatalogController {
 		UserRating ratings1=restTemplate.getForObject("http://localhost:9090/rating/users/" +userId, UserRating.class);
 		
 		return  ratings1.getUserRating().stream().map(rating ->{
-		 Movie movie = restTemplate.getForObject("http://localhost:9092/movie/" + rating.getMovieId(), Movie.class);
+			final String uri ="http://localhost:9092/movie/movieId";
+		 //Movie movie = restTemplate.getForObject("http://localhost:9092/movie/" + rating.getMovieId(), Movie.class);
+			Movie movie = restTemplate.getForObject(uri, Movie.class);
 		 return new CatalogItem(movie.getName(), "village development", rating.getRating());
 	})
 				.collect(Collectors.toList());
@@ -73,7 +76,15 @@ public class CatalogController {
 				})
 				.collect(Collectors.toList());*/
 			
-		
+	/*	@RequestMapping("/{userId}")
+		public List<CatalogItem> getCatalog(@PathVariable(value="userId") String userId){
+			List<Rating> ratings1=restTemplate.getForObject("http://localhost:9090/rating/users/" +userId, UserRating.class).getUserRating();
+			return ratings1.stream().map(rating ->{
+				final String uri ="http://localhost:9092/movie/movieId";
+				Movie forObject = restTemplate.getForObject(uri, Movie.class);
+				return new CatalogItem(forObject.getName(), "village development", rating.getRating());
+			}).collect(Collectors.toList());*/
+
 		
 	}
 }
